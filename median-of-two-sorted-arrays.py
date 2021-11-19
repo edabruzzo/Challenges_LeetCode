4181a32401610e604bd1bc04ac02c5a32981da1a
Emmanuel Abruzzo <emmanuel.oliveira3@gmail.com>
2021-11-19 10:12:09 -0300

    median-of-two-sorted-arrays

---
+++ median-of-two-sorted-arrays.py
@@ -0,0 +1,63 @@
+import heapq
+from typing import List
+
+
+def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+
+    minheap, maxheap = [], []
+
+    '''
+    nums1.length == m
+    nums2.length == n
+    0 <= m <= 1000
+    0 <= n <= 1000
+    1 <= m + n <= 2000
+    -106 <= nums1[i], nums2[i] <= 106
+    '''
+    m = len(nums1)
+    n = len(nums2)
+
+    condicoes_satisfeitas_1 = ( m >=0
+                             and m <= 1000
+                             and n >= 0
+                             and n<=1000
+                             and m+n >=1 and m+n <=2000)
+
+    for index in range(len(nums1)):
+        if nums1[index] >= -10**6 and nums1[index]<=10**6:
+            condicoes_satisfeitas_2 = True
+
+    for index in range(len(nums2)):
+        if nums2[index] >= -10 ** 6 and nums2[index] <= 10 ** 6:
+            condicoes_satisfeitas_3 = True
+
+    condicoes_satisfeitas = (condicoes_satisfeitas_1
+                             and condicoes_satisfeitas_2
+                             and condicoes_satisfeitas_3)
+
+    if condicoes_satisfeitas:
+        p1, p2 = 0, 0
+
+        while p1 < len(nums1) or p2 < len(nums2):
+
+            if p1 == len(nums1):
+                heapq.heappush(minheap, nums2[p2])
+                p2 += 1
+            elif p2 == len(nums2):
+                heapq.heappush(minheap, nums1[p1])
+                p1 += 1
+            elif nums1[p1] < nums2[p2]:
+                heapq.heappush(minheap, nums1[p1])
+                p1 += 1
+            else:
+                heapq.heappush(minheap, nums2[p2])
+                p2 += 1
+            if len(minheap) > len(maxheap):
+                heapq.heappush(maxheap, -heapq.heappop(minheap))
+
+        return (minheap[0] - maxheap[0]) / 2 if len(minheap) == len(maxheap) else -maxheap[0]
+
+
+
+if __name__ == '__main__':
+    pass
--- number-of-valid-words-for-each-puzzle.py
+++ number-of-valid-words-for-each-puzzle.py
@@ -8,6 +8,7 @@ def contarTempo(func):
         print('Tempo de execução: %f' %(fim - inicio))
     return wrapper
 
+
 @contarTempo
 def findNumOfValidWordsSlower(words, puzzles) -> [int]:
     '''
@@ -42,6 +43,7 @@ def findNumOfValidWordsSlower(words, puzzles) -> [int]:
             ans[i] += freq[tuple(sorted(puzzleSubSet))]  # Add answer of that subset for puzzles[i]
 
     print(ans)
+
 
 
 @contarTempo
@@ -99,17 +101,136 @@ def findNumOfValidWords(words, puzzles) -> [int]:
 
 
 
+class Trie:
+    '''
+    https://www.geeksforgeeks.org/trie-insert-and-search/
+    https://www.baeldung.com/trie-java
+
+
+    '''
+
+    def __init__(self):
+        from collections import defaultdict
+        self.chars, self.termino = defaultdict(Trie), 0
+
+    def busca_trie_insert_search(self, palavra):
+        cur = self
+        for character in palavra:
+            cur = cur.chars[character]
+        cur.termino += 1
+
+    def busca_profundidade_recursiva(self, palavra_puzzle, cur, hasFirst):
+        ans = cur.endsHere if hasFirst else 0
+        for character in palavra_puzzle:
+            ans += self.busca_trie_insert_search(palavra_puzzle, cur.chars[character], hasFirst or character == palavra_puzzle[0]) if character in cur.chars else 0
+        return ans
+
+
+@contarTempo
+def findNumOfValidWordsBuscaProfundidade(words: [str], puzzles: [str]) -> [int]:
+    raiz = Trie()
+    for word in words:
+        raiz.mapear_para_dicionario(set(word))
+    print([raiz.busca_profundidade_recursiva(puzzle, raiz, False) for puzzle in puzzles])
+
+
+# Python program for insert and search
+# operation in a Trie
+
+class TrieNode():
+
+    english_alphabet = """a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z""".split(',')
+
+    # Trie node class
+    def __init__(self):
+        self.children = [None] * len(self.english_alphabet)
+        # isEndOfWord is True if node represent the end of the word
+        self.isEndOfWord = False
 
 
 
+class Trie__:
+
+    # Trie data structure class
+    def __init__(self):
+        self.root = self.getNode()
+
+    def getNode(self):
+
+        # Returns new trie node (initialized to NULLs)
+        return TrieNode()
+
+    def _charToIndex(self, ch):
+
+        # private helper function
+        # Converts key current character into index
+        # use only 'a' through 'z' and lower case
+        index = ord(ch) - ord('a')
+        return index
+
+    def insert(self, key):
+
+        # If not present, inserts key into trie
+        # If the key is prefix of trie node,
+        # just marks leaf node
+        pCrawl = self.root
+        length = len(key)
+        for level in range(length):
+            index = self._charToIndex(key[level])
+
+            # if current character is not present
+            if not pCrawl.children[index]:
+                pCrawl.children[index] = self.getNode()
+            pCrawl = pCrawl.children[index]
+
+        # mark last node as leaf
+        pCrawl.isEndOfWord = True
+
+    def search(self, key):
+
+        # Search key in the trie
+        # Returns true if key presents
+        # in trie, else false
+        pCrawl = self.root
+        length = len(key)
+        for level in range(length):
+            index = self._charToIndex(key[level])
+            if not pCrawl.children[index]:
+                return False
+            pCrawl = pCrawl.children[index]
+
+        return pCrawl.isEndOfWord
 
 
+@contarTempo
+def findNumOfValidWordsTrieNode(words, puzzles):
+    # Input keys (use only 'a' through 'z' and lower case)
+    keys = ["the", "a", "there", "anaswe", "any",
+            "by", "their"]
+    output = ["Not present in trie",
+              "Present in trie"]
+
+    # Trie object
+    t = Trie__()
+
+    # Construct trie
+    for key in keys:
+        t.insert(key)
+
+    # Search for different keys
+    print("{} ---- {}".format("the", output[t.search("the")]))
+    print("{} ---- {}".format("these", output[t.search("these")]))
+    print("{} ---- {}".format("their", output[t.search("their")]))
+    print("{} ---- {}".format("thaw", output[t.search("thaw")]))
 
 
 if __name__ == '__main__':
-    words = ["aaaa","asas","able","ability","actt","actor","access"]
+    words = ["aaaa","asas",]
+    words_1 = ["aaaa", "asas", "able", "ability", "actt", "actor", "access"]
     puzzles = ["aboveyz","abrodyz","abslute","absoryz","actresz","gaswxyz"]
-    inicio = time.time()
 
+
+    findNumOfValidWordsTrieNode(words, puzzles)
+    findNumOfValidWordsBuscaProfundidade(words_1,puzzles)
     findNumOfValidWordsSlower(words, puzzles)
     findNumOfValidWords(words, puzzles)
